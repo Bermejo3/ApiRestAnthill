df5ad42b8554ecc3c99704536a0efdd0c71e8f8e
Bermejo3 <alberto.bermejo.corredor@gmail.com>
2021-04-12 13:30:41 +0200

    Anthill

--- apiRestUser.js
+++ apiRestUser.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json())
+app.use(cors())
+
 
 let mysql = require('mysql');
 let connection = mysql.createConnection({
-    database: "Usuarios",
+    database: "Anthill",
     host: "anthill-database.cxgso31trbdb.us-east-2.rds.amazonaws.com",
     user: "kurtybrown",
     password: "z1b23op9$"
